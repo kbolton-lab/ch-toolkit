5c162ed750c74440072a18ac35ae5b292055cf22
indraniel <indraniel@gmail.com>
2023-01-13 09:59:59 -0600

    + flesh out the variant db init subcommand
      - first pass implementation.  still a work in progress

--- chip/dbtools/cli.py
+++ chip/dbtools/cli.py
@@ -1,5 +1,6 @@
-import signal, importlib.resources
+import os, sys, signal
 import click
+from clint.textui import puts, colored
 
 from chip.version import __version__
 
@@ -13,10 +14,14 @@ def cli():
     # http://newbebweb.blogspot.com/2012/02/python-head-ioerror-errno-32-broken.html
     signal.signal(signal.SIGPIPE, signal.SIG_DFL)
 
-@cli.command('init-db', short_help="initialize a SNP/INDEL and SV candidate database")
+@cli.command('init', short_help="initialize a SNP/INDEL and SV candidate database")
 @click.option('--db', 'db_path', default=None, type=click.Path(), required=True,
               help="Path to create sqlite database")
-def init_db(db_path):
-    with importlib.resources.path('chip.resources.sql', 'schema.sql') as f:
-        schema_file = str(f.resolve())
-        click.echo(schema_file)
+@click.option('--sample-name', 'sample_name', type=click.STRING, required=True,
+              help="Sample Name for the variant database")
+def init_db(db_path, sample_name):
+    if os.path.exists(db_path):
+        sys.exit(f"[err] Variant Database '{db_path}' already exists on filesystem!")
+    import chip.dbtools.initialize as init
+    init.create_db(db_path, sample_name)
+    puts(colored.green(f"---> Successfully created variant database ({sample_name}): {db_path}"))
---
+++ chip/dbtools/initialize.py
@@ -0,0 +1,33 @@
+import importlib.resources, sqlite3
+from clint.textui import puts, indent
+
+def create_db(db_path, sample_name):
+    puts("Constructing database...")
+    connection = sqlite3.connect(db_path)
+    with connection, indent(4):
+        create_tables(connection)
+        add_seed_data(connection)
+        add_sample_name(connection, sample_name)
+    connection.close()
+
+def create_tables(connection):
+    puts("Creating tables...")
+    source = importlib.resources.files('chip.resources.sql.variantdb').joinpath('schema.sql')
+    with importlib.resources.as_file(source) as sql:
+        connection.executescript(sql.read_text())
+
+def add_seed_data(connection):
+    puts("Populating seed data..")
+    resource_dir = importlib.resources.files('chip.resources.sql.variantdb.seed')
+    seed_files = [ 'chromosomes.sql' ]
+    with indent(2):
+        for f in seed_files:
+            puts(f"Seed: {f}")
+            source = resource_dir.joinpath(f)
+            with importlib.resources.as_file(source) as sql:
+                connection.executescript(sql.read_text())
+
+def add_sample_name(connection, sample_name):
+    puts(f"Setting sample name to: {sample_name}")
+    sql = "INSERT INTO samples (sample_name) VALUES (?)"
+    connection.execute(sql, (sample_name,))
