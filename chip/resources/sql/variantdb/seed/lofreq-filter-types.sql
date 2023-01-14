545d46f6d11cfb11faf1aa5f4b74e50ccb351a10
Irenaeus <>
2023-01-13 18:21:14 -0600

    Adding Seed for Filter Types

---
+++ chip/resources/sql/variantdb/seed/lofreq-filter-types.sql
@@ -0,0 +1,7 @@
+INSERT INTO lofreq_filter_types (lofreq_filter_type_id, tag) VALUES (1, 'PASS');
+INSERT INTO lofreq_filter_types (lofreq_filter_type_id, tag) VALUES (2, 'min_snvqual_45');
+INSERT INTO lofreq_filter_types (lofreq_filter_type_id, tag) VALUES (3, 'min_indelqual_29');
+INSERT INTO lofreq_filter_types (lofreq_filter_type_id, tag) VALUES (4, 'min_dp_10');
+INSERT INTO lofreq_filter_types (lofreq_filter_type_id, tag) VALUES (5, 'sb_fdr');
+INSERT INTO lofreq_filter_types (lofreq_filter_type_id, tag) VALUES (6, 'min_snvqual_61');
+INSERT INTO lofreq_filter_types (lofreq_filter_type_id, tag) VALUES (7, 'min_indelqual_49');
