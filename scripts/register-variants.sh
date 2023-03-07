ea319a013963bde6424c9824dbf5e058ece9d839
indraniel <indraniel@gmail.com>
2023-03-07 15:46:50 -0600

    + flesh out variant registration jobs a bit more

---
+++ scripts/register-variants.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# INPUTS
+BATCH=$1
+IN_VCF=$2
+REDIS_HOST=$3
+REIDS_PORT=$4
+
+# Command
+VIRTUALENV=${SCRIPT_DIR}/../venv
+BASE_CMD=${VIRTUALENV}/bin/chip-variant-db
+
+# Redis
+
+${BASE_CMD} \
+    --input-vcf=${IN_VCF} \
+    --redis-host=${REDIS_HOST} \
+    --redis-port=${REDIS_PORT} \
+    --batch-number=${BATCH}
