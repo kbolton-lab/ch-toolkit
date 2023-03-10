dd2af5cb3a6ba1b3f9bc711e01d52fa5731f5b7d
indraniel <indraniel@gmail.com>
2023-03-10 17:02:12 -0600

    + remove unneded lofreq and pindel functions/cases

--- chip/vdbtools/importer.py
+++ chip/vdbtools/importer.py
@@ -2,27 +2,17 @@ import sys
 
 def import_vcf(db_path, input_vcf, caller):
     dispatch = {
-        'lofreq'  : import_lofreq,
         'mutect'  : import_mutect,
-        'vardict' : import_vardict,
-        'pindel'  : import_pindel,
+        'vardict' : import_vardict
     }
     function = dispatch[caller]
     function(db_path, input_vcf)
-
-def import_lofreq(db_path, input_vcf):
-    import chip.vdbtools.importers.caller.lofreq as lofreq
-    ingestor = lofreq.Ingestor(db_path, input_vcf)
-    ingestor.ingest_vcf()
 
 def import_mutect(db_path, input_vcf):
     sys.exit("[err] Please implement me -- import_mutect !")
 
 def import_vardict(db_path, input_vcf):
     sys.exit("[err] Please implement me -- import_vardict !")
-
-def import_pindel(db_path, input_vcf):
-    sys.exit("[err] Please implement me -- import_pindel !")
 
 def import_variant_batch(duckdb_file, redis_host, redis_port, batch_number, debug):
     import chip.vdbtools.importers.variants as variants
