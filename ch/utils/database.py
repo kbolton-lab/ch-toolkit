b5fa45854349bcef36e76155b6fb2f030956de24
indraniel <indraniel@gmail.com>
2023-06-02 17:15:35 -0500

    + remove more redis usage
      - no longer needed at the moment

--- ch/utils/database.py
+++ ch/utils/database.py
@@ -2,12 +2,7 @@ import os, sys
 
 import ch.utils.logger as log
 
-import redis
 import duckdb
-
-def redis_connect(host, port):
-    r = redis.Redis(host=host, port=port, db=0)
-    return r
 
 def duckdb_connect_rw(db_file, clobber):
     if os.path.exists(db_file) and clobber == True:
